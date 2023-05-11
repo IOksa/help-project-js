6068bc20eb4e35aea44aa0bdce0569733aed006f
IOksa <117656767+IOksa@users.noreply.github.com>
2023-05-11 17:08:13 +0000

    Deploying to gh-pages from @ IOksa/help-project-js@949601bc7c73318f421ec65f6107f021b381c34f 🚀

---
+++ index.0b2b735c.js
@@ -0,0 +1,2 @@
+fetchCountries;console.log(fetchTopBooks());
+//# sourceMappingURL=index.0b2b735c.js.map
---
+++ index.4e667f51.js
@@ -0,0 +1,2 @@
+!function(){fetchCountries;console.log(fetchTopBooks())}();
+//# sourceMappingURL=index.4e667f51.js.map
